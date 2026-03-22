3335ca4073bae994cfef9d433368a2d1c1e93d98
steven0710 <stevenhujuly10@gmail.com>
2026-03-21 20:26:47 -0500

    made username optional for now

--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const registerUserSchema = z
   .object({
-    username: z.string().min(1, "Username is required"),
+    // username: z.string().min(1, "Username is required"),
     email: z.string().email("Invalid email"),
     password: z.string().min(6, "Password must be at least 6 characters"),
   })
